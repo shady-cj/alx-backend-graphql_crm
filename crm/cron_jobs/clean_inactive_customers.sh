56eb72a3779ea96a2c0fcc074b1bbc2f1e9574cb
shady-cj <petersp2000@gmail.com>
2025-10-30 15:24:08 +0100

    switch to using manage.py shell

--- crm/cron_jobs/clean_inactive_customers.sh
+++ crm/cron_jobs/clean_inactive_customers.sh
@@ -2,4 +2,6 @@
 # This script cleans up inactive customers from the CRM database.
 # It removes customers with no orders since a year ago.
 
-/c/Users/SICKBOY/ceejay/alx-backend-pro/alx-backend-graphql_crm/.env/Scripts/python /c/Users/SICKBOY/ceejay/alx-backend-pro/alx-backend-graphql_crm/manage.py clean_inactive_customers
+# /c/Users/SICKBOY/ceejay/alx-backend-pro/alx-backend-graphql_crm/.env/Scripts/python /c/Users/SICKBOY/ceejay/alx-backend-pro/alx-backend-graphql_crm/manage.py clean_inactive_customers
+
+/c/Users/SICKBOY/ceejay/alx-backend-pro/alx-backend-graphql_crm/.env/Scripts/python /c/Users/SICKBOY/ceejay/alx-backend-pro/alx-backend-graphql_crm/manage.py shell -c "from crm.models import Customer; from django.utils import timezone; from datetime import timedelta; cutoff_date = timezone.now() - timedelta(days=365); inactive_customers = Customer.objects.filter(order__order_date__lte=cutoff_date); count = inactive_customers.count(); inactive_customers.delete(); print(f'Deleted {count} inactive customers.')"
