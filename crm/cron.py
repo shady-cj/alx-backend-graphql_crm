73483782b92d0cb5390f9a93c946b9220784d1cf
shady-cj <petersp2000@gmail.com>
2025-10-30 20:59:11 +0100

    crm heartbeat log

--- crm/cron.py
+++ crm/cron.py
@@ -1,10 +1,35 @@
 import datetime
 
+from gql import Client, gql
+from gql.transport.aiohttp import AIOHTTPTransport
+
+# Select your transport with a defined url endpoint
+transport = AIOHTTPTransport(url="http://localhost:8000/graphql")
+
+# Create a GraphQL client using the defined transport
+client = Client(transport=transport)
+
+
+
+
 def log_crm_heartbeat():
     """
     DD/MM/YYYY-HH:MM:SS CRM is alive
     """
 
     datetime_now = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
-    with open("/tmp/crm_heartbeat.log", "a") as log_file:
+    with open("/tmp/crm_heartbeat_log.txt", "a") as log_file:
         log_file.write(f"{datetime_now} CRM is alive\n")
+
+    # Provide a GraphQL query
+    query = gql(
+        """
+        query {
+            hello
+        }
+    """
+    )
+
+    # Execute the query
+    response = client.execute(query)
+    print(response)
